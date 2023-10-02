bc2af2916beb50a1b4fa05d34db47946daa86078
majedawwad <74358245+majedawwad@users.noreply.github.com>
2023-10-02 10:47:43 +0300

    ++

--- src/MFAE.Jobs.Web.Mvc/wwwroot/view-resources/Areas/App/Views/JobAdvertisements/Index.js
+++ src/MFAE.Jobs.Web.Mvc/wwwroot/view-resources/Areas/App/Views/JobAdvertisements/Index.js
@@ -3,9 +3,9 @@
 
         var _$jobAdvertisementsTable = $('#JobAdvertisementsTable');
         var _jobAdvertisementsService = abp.services.app.jobAdvertisements;
-		var _entityTypeFullName = 'MFAE.Jobs.ApplicationForm.JobAdvertisement';
-        
-       var $selectedDate = {
+        var _entityTypeFullName = 'MFAE.Jobs.ApplicationForm.JobAdvertisement';
+
+        var $selectedDate = {
             startDate: null,
             endDate: null,
         }
@@ -18,33 +18,31 @@
             autoUpdateInput: false,
             singleDatePicker: true,
             locale: abp.localization.currentLanguage.name,
-            format: 'L',
         })
-        .on("apply.daterangepicker", (ev, picker) => {
-            $selectedDate.startDate = picker.startDate;
-            getJobAdvertisements();
-        })
-        .on('cancel.daterangepicker', function (ev, picker) {
-            $(this).val("");
-            $selectedDate.startDate = null;
-            getJobAdvertisements();
-        });
+            .on("apply.daterangepicker", (ev, picker) => {
+                $selectedDate.startDate = picker.startDate;
+                getJobAdvertisements();
+            })
+            .on('cancel.daterangepicker', function (ev, picker) {
+                $(this).val("");
+                $selectedDate.startDate = null;
+                getJobAdvertisements();
+            });
 
         $('.endDate').daterangepicker({
             autoUpdateInput: false,
             singleDatePicker: true,
             locale: abp.localization.currentLanguage.name,
-            format: 'L',
         })
-        .on("apply.daterangepicker", (ev, picker) => {
-            $selectedDate.endDate = picker.startDate;
-            getJobAdvertisements();
-        })
-        .on('cancel.daterangepicker', function (ev, picker) {
-            $(this).val("");
-            $selectedDate.endDate = null;
-            getJobAdvertisements();
-        });
+            .on("apply.daterangepicker", (ev, picker) => {
+                $selectedDate.endDate = picker.startDate;
+                getJobAdvertisements();
+            })
+            .on('cancel.daterangepicker', function (ev, picker) {
+                $(this).val("");
+                $selectedDate.endDate = null;
+                getJobAdvertisements();
+            });
 
         var _permissions = {
             create: abp.auth.hasPermission('Pages.JobAdvertisements.Create'),
@@ -52,20 +50,21 @@
             'delete': abp.auth.hasPermission('Pages.JobAdvertisements.Delete')
         };
 
-         var _createOrEditModal = new app.ModalManager({
-                    viewUrl: abp.appPath + 'App/JobAdvertisements/CreateOrEditModal',
-                    scriptUrl: abp.appPath + 'view-resources/Areas/App/Views/JobAdvertisements/_CreateOrEditModal.js',
-                    modalClass: 'CreateOrEditJobAdvertisementModal'
-                });
-                   
+        var _createOrEditModal = new app.ModalManager({
+            viewUrl: abp.appPath + 'App/JobAdvertisements/CreateOrEditModal',
+            scriptUrl: abp.appPath + 'view-resources/Areas/App/Views/JobAdvertisements/_CreateOrEditModal.js',
+            modalClass: 'CreateOrEditJobAdvertisementModal',
+            modalSize: 'modal-xl'
+        });
 
-		 var _viewJobAdvertisementModal = new app.ModalManager({
+
+        var _viewJobAdvertisementModal = new app.ModalManager({
             viewUrl: abp.appPath + 'App/JobAdvertisements/ViewjobAdvertisementModal',
             modalClass: 'ViewJobAdvertisementModal'
         });
 
-		        var _entityTypeHistoryModal = app.modals.EntityTypeHistoryModal.create();
-		        function entityHistoryIsEnabled() {
+        var _entityTypeHistoryModal = app.modals.EntityTypeHistoryModal.create();
+        function entityHistoryIsEnabled() {
             return abp.auth.hasPermission('Pages.Administration.AuditLogs') &&
                 abp.custom.EntityHistory &&
                 abp.custom.EntityHistory.IsEnabled &&
@@ -76,14 +75,14 @@
             if ($selectedDate.startDate == null) {
                 return null;
             }
-            return $selectedDate.startDate.format("YYYY-MM-DDT00:00:00Z"); 
+            return $selectedDate.startDate.format("YYYY-MM-DDT00:00:00Z");
         }
-        
+
         var getMaxDateFilter = function (element) {
             if ($selectedDate.endDate == null) {
                 return null;
             }
-            return $selectedDate.endDate.format("YYYY-MM-DDT23:59:59Z"); 
+            return $selectedDate.endDate.format("YYYY-MM-DDT23:59:59Z");
         }
 
         var dataTable = _$jobAdvertisementsTable.DataTable({
@@ -94,18 +93,18 @@
                 ajaxFunction: _jobAdvertisementsService.getAll,
                 inputFilter: function () {
                     return {
-					filter: $('#JobAdvertisementsTableFilter').val(),
-					descriptionFilter: $('#DescriptionFilterId').val(),
-					advertisementIdFilter: $('#AdvertisementIdFilterId').val(),
-					minAdvertisementDateFilter:  getDateFilter($('#MinAdvertisementDateFilterId')),
-					maxAdvertisementDateFilter:  getMaxDateFilter($('#MaxAdvertisementDateFilterId')),
-					minFromDateFilter:  getDateFilter($('#MinFromDateFilterId')),
-					maxFromDateFilter:  getMaxDateFilter($('#MaxFromDateFilterId')),
-					minToDateFilter:  getDateFilter($('#MinToDateFilterId')),
-					maxToDateFilter:  getMaxDateFilter($('#MaxToDateFilterId')),
-					minAllowedAgeFilter: $('#MinAllowedAgeFilterId').val(),
-					maxAllowedAgeFilter: $('#MaxAllowedAgeFilterId').val(),
-					isActiveFilter: $('#IsActiveFilterId').val()
+                        filter: $('#JobAdvertisementsTableFilter').val(),
+                        descriptionFilter: $('#DescriptionFilterId').val(),
+                        advertisementIdFilter: $('#AdvertisementIdFilterId').val(),
+                        minAdvertisementDateFilter: getDateFilter($('#MinAdvertisementDateFilterId')),
+                        maxAdvertisementDateFilter: getMaxDateFilter($('#MaxAdvertisementDateFilterId')),
+                        minFromDateFilter: getDateFilter($('#MinFromDateFilterId')),
+                        maxFromDateFilter: getMaxDateFilter($('#MaxFromDateFilterId')),
+                        minToDateFilter: getDateFilter($('#MinToDateFilterId')),
+                        maxToDateFilter: getMaxDateFilter($('#MaxToDateFilterId')),
+                        minAllowedAgeFilter: $('#MinAllowedAgeFilterId').val(),
+                        maxAllowedAgeFilter: $('#MaxAllowedAgeFilterId').val(),
+                        isActiveFilter: $('#IsActiveFilterId').val()
                     };
                 }
             },
@@ -129,108 +128,108 @@
                         cssClass: 'btn btn-brand dropdown-toggle',
                         text: '<i class="fa fa-cog"></i> ' + app.localize('Actions') + ' <span class="caret"></span>',
                         items: [
-						{
+                            {
                                 text: app.localize('View'),
                                 action: function (data) {
                                     _viewJobAdvertisementModal.open({ id: data.record.jobAdvertisement.id });
                                 }
-                        },
-						{
-                            text: app.localize('Edit'),
-                            visible: function () {
-                                return _permissions.edit;
                             },
-                            action: function (data) {
-                            _createOrEditModal.open({ id: data.record.jobAdvertisement.id });                                
-                            }
-                        },
-                        {
-                            text: app.localize('History'),
-                            iconStyle: 'fas fa-history mr-2',
-                            visible: function () {
-                                return entityHistoryIsEnabled();
+                            {
+                                text: app.localize('Edit'),
+                                visible: function () {
+                                    return _permissions.edit;
+                                },
+                                action: function (data) {
+                                    _createOrEditModal.open({ id: data.record.jobAdvertisement.id });
+                                }
                             },
-                            action: function (data) {
-                                _entityTypeHistoryModal.open({
-                                    entityTypeFullName: _entityTypeFullName,
-                                    entityId: data.record.jobAdvertisement.id
-                                });
-                            }
-						}, 
-						{
-                            text: app.localize('Delete'),
-                            visible: function () {
-                                return _permissions.delete;
+                            {
+                                text: app.localize('History'),
+                                iconStyle: 'fas fa-history mr-2',
+                                visible: function () {
+                                    return entityHistoryIsEnabled();
+                                },
+                                action: function (data) {
+                                    _entityTypeHistoryModal.open({
+                                        entityTypeFullName: _entityTypeFullName,
+                                        entityId: data.record.jobAdvertisement.id
+                                    });
+                                }
                             },
-                            action: function (data) {
-                                deleteJobAdvertisement(data.record.jobAdvertisement);
-                            }
-                        }]
+                            {
+                                text: app.localize('Delete'),
+                                visible: function () {
+                                    return _permissions.delete;
+                                },
+                                action: function (data) {
+                                    deleteJobAdvertisement(data.record.jobAdvertisement);
+                                }
+                            }]
                     }
                 },
-					{
-						targets: 2,
-						 data: "jobAdvertisement.description",
-						 name: "description"   
-					},
-					{
-						targets: 3,
-						 data: "jobAdvertisement.advertisementId",
-						 name: "advertisementId"   
-					},
-					{
-						targets: 4,
-						 data: "jobAdvertisement.advertisementDate",
-						 name: "advertisementDate" ,
-					render: function (advertisementDate) {
-						if (advertisementDate) {
-							return moment(advertisementDate).format('L');
-						}
-						return "";
-					}
-			  
-					},
-					{
-						targets: 5,
-						 data: "jobAdvertisement.fromDate",
-						 name: "fromDate" ,
-					render: function (fromDate) {
-						if (fromDate) {
-							return moment(fromDate).format('L');
-						}
-						return "";
-					}
-			  
-					},
-					{
-						targets: 6,
-						 data: "jobAdvertisement.toDate",
-						 name: "toDate" ,
-					render: function (toDate) {
-						if (toDate) {
-							return moment(toDate).format('L');
-						}
-						return "";
-					}
-			  
-					},
-					{
-						targets: 7,
-						 data: "jobAdvertisement.allowedAge",
-						 name: "allowedAge"   
-					},
-					{
-						targets: 8,
-						 data: "jobAdvertisement.isActive",
-						 name: "isActive"  ,
-						render: function (isActive) {
-							if (isActive) {
-								return '<div class="text-center"><i class="fa fa-check text-success" title="True"></i></div>';
-							}
-							return '<div class="text-center"><i class="fa fa-times-circle" title="False"></i></div>';
-					}
-			 
-					}
+                {
+                    targets: 2,
+                    data: "jobAdvertisement.description",
+                    name: "description"
+                },
+                {
+                    targets: 3,
+                    data: "jobAdvertisement.advertisementId",
+                    name: "advertisementId"
+                },
+                {
+                    targets: 4,
+                    data: "jobAdvertisement.advertisementDate",
+                    name: "advertisementDate",
+                    render: function (advertisementDate) {
+                        if (advertisementDate) {
+                            return moment(advertisementDate).format('L');
+                        }
+                        return "";
+                    }
+
+                },
+                {
+                    targets: 5,
+                    data: "jobAdvertisement.fromDate",
+                    name: "fromDate",
+                    render: function (fromDate) {
+                        if (fromDate) {
+                            return moment(fromDate).format('L');
+                        }
+                        return "";
+                    }
+
+                },
+                {
+                    targets: 6,
+                    data: "jobAdvertisement.toDate",
+                    name: "toDate",
+                    render: function (toDate) {
+                        if (toDate) {
+                            return moment(toDate).format('L');
+                        }
+                        return "";
+                    }
+
+                },
+                {
+                    targets: 7,
+                    data: "jobAdvertisement.allowedAge",
+                    name: "allowedAge"
+                },
+                {
+                    targets: 8,
+                    data: "jobAdvertisement.isActive",
+                    name: "isActive",
+                    render: function (isActive) {
+                        if (isActive) {
+                            return '<div class="text-center"><i class="fa fa-check text-success" title="True"></i></div>';
+                        }
+                        return '<div class="text-center"><i class="fa fa-times-circle" title="False"></i></div>';
+                    }
+
+                }
             ]
         });
 
@@ -255,7 +254,7 @@
             );
         }
 
-		$('#ShowAdvancedFiltersSpan').click(function () {
+        $('#ShowAdvancedFiltersSpan').click(function () {
             $('#ShowAdvancedFiltersSpan').hide();
             $('#HideAdvancedFiltersSpan').show();
             $('#AdvacedAuditFiltersArea').slideDown();
@@ -269,24 +268,24 @@
 
         $('#CreateNewJobAdvertisementButton').click(function () {
             _createOrEditModal.open();
-        });        
+        });
 
-		$('#ExportToExcelButton').click(function () {
+        $('#ExportToExcelButton').click(function () {
             _jobAdvertisementsService
                 .getJobAdvertisementsToExcel({
-				filter : $('#JobAdvertisementsTableFilter').val(),
-					descriptionFilter: $('#DescriptionFilterId').val(),
-					advertisementIdFilter: $('#AdvertisementIdFilterId').val(),
-					minAdvertisementDateFilter:  getDateFilter($('#MinAdvertisementDateFilterId')),
-					maxAdvertisementDateFilter:  getMaxDateFilter($('#MaxAdvertisementDateFilterId')),
-					minFromDateFilter:  getDateFilter($('#MinFromDateFilterId')),
-					maxFromDateFilter:  getMaxDateFilter($('#MaxFromDateFilterId')),
-					minToDateFilter:  getDateFilter($('#MinToDateFilterId')),
-					maxToDateFilter:  getMaxDateFilter($('#MaxToDateFilterId')),
-					minAllowedAgeFilter: $('#MinAllowedAgeFilterId').val(),
-					maxAllowedAgeFilter: $('#MaxAllowedAgeFilterId').val(),
-					isActiveFilter: $('#IsActiveFilterId').val()
-				})
+                    filter: $('#JobAdvertisementsTableFilter').val(),
+                    descriptionFilter: $('#DescriptionFilterId').val(),
+                    advertisementIdFilter: $('#AdvertisementIdFilterId').val(),
+                    minAdvertisementDateFilter: getDateFilter($('#MinAdvertisementDateFilterId')),
+                    maxAdvertisementDateFilter: getMaxDateFilter($('#MaxAdvertisementDateFilterId')),
+                    minFromDateFilter: getDateFilter($('#MinFromDateFilterId')),
+                    maxFromDateFilter: getMaxDateFilter($('#MaxFromDateFilterId')),
+                    minToDateFilter: getDateFilter($('#MinToDateFilterId')),
+                    maxToDateFilter: getMaxDateFilter($('#MaxToDateFilterId')),
+                    minAllowedAgeFilter: $('#MinAllowedAgeFilterId').val(),
+                    maxAllowedAgeFilter: $('#MaxAllowedAgeFilterId').val(),
+                    isActiveFilter: $('#IsActiveFilterId').val()
+                })
                 .done(function (result) {
                     app.downloadTempFile(result);
                 });
@@ -296,32 +295,32 @@
             getJobAdvertisements();
         });
 
-		$('#GetJobAdvertisementsButton').click(function (e) {
+        $('#GetJobAdvertisementsButton').click(function (e) {
             e.preventDefault();
             getJobAdvertisements();
         });
 
-		$(document).keypress(function(e) {
-		  if(e.which === 13) {
-			getJobAdvertisements();
-		  }
-		});
+        $(document).keypress(function (e) {
+            if (e.which === 13) {
+                getJobAdvertisements();
+            }
+        });
 
-        $('.reload-on-change').change(function(e) {
-			getJobAdvertisements();
-		});
+        $('.reload-on-change').change(function (e) {
+            getJobAdvertisements();
+        });
 
-        $('.reload-on-keyup').keyup(function(e) {
-			getJobAdvertisements();
-		});
+        $('.reload-on-keyup').keyup(function (e) {
+            getJobAdvertisements();
+        });
 
         $('#btn-reset-filters').click(function (e) {
             $('.reload-on-change,.reload-on-keyup,#MyEntsTableFilter').val('');
             getJobAdvertisements();
         });
-		
-		
-		
+
+
+
 
     });
 })();
